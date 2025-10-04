57ee8a2dc20f7eb7336bff8a083c281a08095b33
fabohax <40230@pm.me>
2025-10-03 21:25:13 -0300

    .

---
+++ app/bafybeid6oo6es4erf2etwtxmpac3z2v7swfamkcslqq2q55yseshl3wwoa/page.tsx
@@ -0,0 +1,7 @@
+import { redirect } from 'next/navigation';
+
+export default function Page() {
+	// Server-side redirect
+	redirect('https://cholo.mypinata.cloud/ipfs/bafybeid6oo6es4erf2etwtxmpac3z2v7swfamkcslqq2q55yseshl3wwoa');
+}
+
